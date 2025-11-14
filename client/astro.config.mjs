11a9dec62c2a26308dfe3f73f45e92aa14aeeed5
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-11-14 15:32:36 -0500

    prueba

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -24,7 +24,6 @@ export default defineConfig({
       "import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY": JSON.stringify(process.env.PUBLIC_CLERK_PUBLISHABLE_KEY),
       "import.meta.env.PUBLIC_URL_API": JSON.stringify(process.env.PUBLIC_URL_API),
       "import.meta.env.PUBLIC_API_URL_LOGIN": JSON.stringify(process.env.PUBLIC_API_URL_LOGIN),
-      "import.meta.env.PUBLIC_CLERK_FRONTEND_API": JSON.stringify(process.env.PUBLIC_CLERK_FRONTEND_API),
     },
   },
 
@@ -35,11 +34,6 @@ export default defineConfig({
       localization: esES,
       publishableKey: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
       secretKey: process.env.CLERK_SECRET_KEY,
-
-      // ✅ NUEVA CONFIGURACIÓN
-      clerkJSUrl: "https://clerk.serviredgane.cloud/npm/@clerk/clerk-js@5/dist/clerk.browser.js",
-      domain: "clerk.serviredgane.cloud",
-      proxyUrl: "https://clerk.serviredgane.cloud",
     }),
   ],
 });
--- client/src/utils/constans.ts
+++ client/src/utils/constans.ts
@@ -14,7 +14,3 @@ export const API_URL_LOGIN = import.meta.env.PUBLIC_API_URL_LOGIN;
 export const apiUrl = import.meta.env.PUBLIC_URL_API;
 export const apiLoginUrl = import.meta.env.PUBLIC_API_URL_LOGIN;
 
-// --- Secret del backend
-// ⚠️ IMPORTANTE: Esto solo existe en el servidor. NO en el navegador.
-// En el cliente siempre vendrá "undefined" (esto es correcto).
-export const authSecret = import.meta.env.CLERK_SECRET_KEY;
