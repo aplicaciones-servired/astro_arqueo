df2feaeced62bd2e76c1bc5453e557613a30bab0
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-11-20 10:34:53 -0500

    arreglar ruta

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(log('dev'));
 app.use(arqueoRoute);
 app.use('/', loginRouter );
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
-
+// .
