b317547709bece8f5fb8440fdbd1310e9ca13b92
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-11-20 16:18:43 -0500

    no trae los datos

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(cors());
 app.use(log('dev'));
 
-app.use('/api', arqueoRoute);
-app.use('/api', loginRouter);
+app.use(arqueoRoute);
+app.use(loginRouter);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}`);
