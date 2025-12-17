2b73548116b9e1c591dbe1ef28f4e4f971605213
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2025-12-17 12:50:21 -0500

    refactor: Mejora del diseño del formulario de Inserción de Arqueo Manual

--- client/src/components/Arqueo/ArqueoManual/InserArqueoManual.tsx
+++ client/src/components/Arqueo/ArqueoManual/InserArqueoManual.tsx
@@ -34,62 +34,56 @@ export const InserArqueoManual = () => {
     }
 
     return (
-        <section className="container px-4 mt-5 ">
-            <form onSubmit={handleSubmit}>
-                <div className="mb-6 p-4 bg-white border border-indigo-200 rounded-lg shadow-lg shadow-blue-300/50">
-                    <h3 className="text-lg font-medium text-gray-700 mb-4 text-center">
-                        Insertar Arqueo Manual
-                    </h3>
+        <section className=" flex items-center justify-center px-4 text-center">
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center gap-4 p-6 w-full max-w-md bg-white border border-indigo-200 rounded-lg shadow-lg shadow-blue-300/50"
+            >
+                <h3 className="text-lg font-medium text-gray-700 text-center">
+                    Insertar Arqueo Manual
+                </h3>
 
-                    <div className="flex flex-col md:flex-row gap-4">
-                        <div className="flex-1">
-                            <label className="block text-sm font-medium text-gray-700 mb-2">
-                                Punto de Venta
-                                <input
-                                    name="puntodeventa"
-                                    type="text"
-                                    placeholder="Punto de Venta"
-                                    value={form.puntodeventa}
-                                    onChange={handleChange}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-                                />
-                            </label>
-                        </div>
+                <div className="w-full flex flex-col gap-4">
+                    <label className="block text-sm font-medium text-gray-700">
+                        Punto de Venta
+                        <input
+                            name="puntodeventa"
+                            type="text"
+                            placeholder="ingresar punto de venta"
+                            value={form.puntodeventa}
+                            onChange={handleChange}
+                            className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                        />
+                    </label>
 
-                        <div className="flex-1">
-                            <label className="block text-sm font-medium text-gray-700 mb-2">
-                                Nombre del Responsable
-                                <input
-                                    name="nombre"
-                                    type="text"
-                                    placeholder="Nombre del que realizo el arqueo"
-                                    value={form.nombre}
-                                    onChange={handleChange}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-                                />
-                            </label>
-                        </div>
+                    <label className="block text-sm font-medium text-gray-700">
+                        Nombre del Responsable
+                        <input
+                            name="nombre"
+                            type="text"
+                            placeholder="ingresar nombre del responsable"
+                            value={form.nombre}
+                            onChange={handleChange}
+                            className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                        />
+                    </label>
 
-                        <div className="flex-1">
-                            <label className="block text-sm font-medium text-gray-700 mb-2">
-                                Documento del Responsable
-                                <input
-                                    name="documento"
-                                    type="text"
-                                    placeholder="Documento del que realizo el arqueo"
-                                    value={form.documento}
-                                    onChange={handleChange}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-                                />
-                            </label>
-                        </div>
-
-                        <div className="flex items-end">
-                            <Button >Insertar Arqueo</Button>
-                        </div>
-                    </div>
+                    <label className="block text-sm font-medium text-gray-700">
+                        Documento del Responsable
+                        <input
+                            name="documento"
+                            type="text"
+                            placeholder="ingresar documento del responsable"
+                            value={form.documento}
+                            onChange={handleChange}
+                            className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                        />
+                    </label>
                 </div>
+
+                <Button>Insertar Arqueo</Button>
             </form>
         </section>
     );
+
 };
