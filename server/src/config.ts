9e233903e69a06cf3c4ab8f90980d1dc431889ab
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-05-07 09:21:16 -0500

    feat: eliminar la opción --env-file en el comando de inicio y ajustar la configuración del puerto desde .env

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,2 +1,4 @@
-//puerto en el cual se ejecutara el servidor
-export const PORT = 3000;
+import 'dotenv/config';
+
+// Puerto en el cual se ejecutara el servidor
+export const PORT = Number(process.env.PORT) || 3000;
