cb6b16f6a0a8f720ba3559092af7c2a26fe76f05
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-05-07 09:34:14 -0500

    feat: restablecer la configuración del puerto a un valor fijo y eliminar la carga de dotenv

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,4 +1,2 @@
-import 'dotenv/config';
-
-// Puerto en el cual se ejecutara el servidor
-export const PORT = Number(process.env.PORT) || 3000;
+//puerto en el cual se ejecutara el servidor
+export const PORT = 3000;
